f655c3a83c7fa3e4fce114eef0a63474a5a5c3cb
João Sequeira <joaosequeira28@hotmail.com>
2022-07-15 02:16:04 +0100

    config

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,11 +1,11 @@
 var Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-    "df0ehl0ofju1b4",
-    "zayoiikmtnxnqg",
-    "4b221b3d3bbcd1a764c6fe910e5e74b9c3082687ffe1b2822c73657a6b9f100e   ",
+    "d41qisifaueaqe",
+    "d41qisifaueaqe",
+    "f84d6a7a5002caab6474d509eeecf199e2fbea58c5ef6445d777fd530afa98ea",
     {
-        host: "ec2-34-247-72-29.eu-west-1.compute.amazonaws.com",
+        host: "ec2-99-80-170-190.eu-west-1.compute.amazonaws.com",
         port: "5432",
         dialect: "postgres",
         dialectOptions: {
