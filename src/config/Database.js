a0a1c4cd9567501f490bab9159621c98562ef9e7
Raquelmeida <raquelalmeida-15@outlook.pt>
2022-07-17 12:30:55 +0100

    update database

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,18 +1,13 @@
 var Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-    "d41qisifaueaqe",
-    "d41qisifaueaqe",
-    "f84d6a7a5002caab6474d509eeecf199e2fbea58c5ef6445d777fd530afa98ea",
+    "pi3_teste_entrega",
+    "postgres",
+    "ola",
     {
-        host: "ec2-99-80-170-190.eu-west-1.compute.amazonaws.com",
+        host: "localhost",
         port: "5432",
         dialect: "postgres",
-        dialectOptions: {
-            ssl:{
-                rejectUnauthorized:false
-            }
-        }
     }
 );
 
