5ab6609d0a0abc5b4a7e3fbd232cf5a609cce448
Joao Sequeira <joaosequeira28@hotmail.com>
2022-07-18 16:50:04 +0100

    make it better

--- src/config/Database.js
+++ src/config/Database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
     'jqsflgimusijhy',
     'c4bd8da8290c7d3ecaf249874b1e2185c8092016997ae41374b74fe9cdb55adb',
     {
-        host: 'postgres://jqsflgimusijhy:c4bd8da8290c7d3ecaf249874b1e2185c8092016997ae41374b74fe9cdb55adb@ec2-54-87-179-4.compute-1.amazonaws.com:5432/d27ue6hqakbqiu',
+        host: 'ec2-54-87-179-4.compute-1.amazonaws.com',
         port: "5432",
         dialect: "postgres",
         dialectOptions: {
